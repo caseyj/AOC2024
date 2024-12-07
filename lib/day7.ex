d1342af08d9c5801d6b2ebda76649f110c429871
John Casey <johnny@Johns-Laptop.local>
2024-12-07 18:54:35 -0500

    Day 7 runs much faster after applying an L1 sort, still getting wrong answer on personal input data but not the sample data

--- lib/day7.ex
+++ lib/day7.ex
@@ -26,18 +26,27 @@ defmodule Day7 do
     val1*val2
   end
 
+  def cat(val1, val2) do
+    Utils.to_int("#{val1}#{val2}")
+  end
+
   def operation_decider(val1, val2, op) do
     case op do
       x when x == :sum -> sum(val1,val2)
       x when x == :mult -> mult(val1,val2)
+      x when x == :cat -> cat(val1,val2)
     end
   end
 
-  def build_options(val1,val2, index) do
-    [{val1, val2, :sum, index},{val1,val2,:mult, index}]
+  def build_options(val1,val2, index, options_list) do
+    Enum.map(options_list, fn x -> {val1, val2, x, index} end)
   end
 
-  def operation_runner(target_value, input_tuple,remaining_operations) do
+  def l_one_dist(target, val) do
+    target-elem(val,0)
+  end
+
+  def operation_runner(target_value, input_tuple,remaining_operations, options_list) do
     if remaining_operations == [] do
       false
     else
@@ -49,45 +58,66 @@ defmodule Day7 do
         starting_index = elem(next_op, 3)
         n_index = starting_index+1
         if n_index < tuple_size(input_tuple) do
-          options = build_options(result, elem(input_tuple, n_index), n_index)
-          operation_runner(target_value, input_tuple, tl(remaining_operations)++options)
+          options = tl(remaining_operations)++build_options(result, elem(input_tuple, n_index), n_index, options_list)
+          operation_runner(target_value, input_tuple, Enum.sort(options, &(l_one_dist(target_value, &1) <= l_one_dist(target_value,&2))) , options_list)
         else
-          operation_runner(target_value, input_tuple, tl(remaining_operations))
+          operation_runner(target_value, input_tuple, tl(remaining_operations), options_list)
         end
       end
     end
   end
 
-  def operation_runner(target_value, input_tuple) do
-    operation_runner(target_value, input_tuple, build_options(elem(input_tuple,0), elem(input_tuple,1), 1))
+  def operation_runner(target_value, input_tuple, options_list) do
+    operation_runner(target_value, input_tuple, build_options(elem(input_tuple,0), elem(input_tuple,1), 1, options_list), options_list)
   end
 
-  def string_to_computation(str) do
+  def numeric_components_tuple(str) do
     comps = parse_line_to_components(str)
     left_side = Utils.to_int(hd(comps))
     right_side = parse_string_components(hd(tl(comps)))
-    if operation_runner(left_side, right_side) do
-      left_side
-    else
-      0
-    end
+    {left_side, right_side}
   end
 
   def convert_input_str_to_list(str) do
-    String.split(str, "\n", trim: true)
+    Enum.reduce(String.split(str, "\n", trim: true), [], fn line,acc ->
+      acc++[numeric_components_tuple(line)]
+    end)
   end
 
-  def count_successful_operations(str) do
-    Enum.reduce(
-      convert_input_str_to_list(str), 0, fn line, acc -> acc+string_to_computation(line) end)
+  def count_successful_operations(lines, options_list, correctness_tracker) do
+    Enum.reduce(lines, correctness_tracker, fn line, acc ->
+        left_side = elem(line, 0)
+        right_side = elem(line, 1)
+        result = operation_runner(left_side, right_side, options_list)
+        if result do
+          upd = Map.get(acc, :correct)++[{left_side, right_side}]
+          Map.put(acc, :correct, upd)
+        else
+          upd = Map.get(acc, :incorrect)++[{left_side, right_side}]
+          Map.put(acc, :incorrect, upd)
+        end
+      end)
+
   end
+
+  def count_correct(correct_map) do
+    Enum.reduce(Map.get(correct_map, :correct), 0, fn x, acc -> acc+ elem(x,0) end)
+  end
+
+
 
   def part1(filename) do
     {:ok, content} = File.read(filename)
-    count_successful_operations(content)
+
+    count_correct(count_successful_operations(convert_input_str_to_list(content), [:sum, :mult], %{:correct=>[],:incorrect=>[]}))
   end
   def part2(filename) do
-    IO.puts("NOT YET IMPLEMENTED")
+    {:ok, content} = File.read(filename)
+    correctness_tracker = %{:correct=>[],:incorrect=>[]}
+    parsed_input = convert_input_str_to_list(content)
+    initial_correct_track = count_successful_operations(parsed_input, [:sum, :mult], correctness_tracker)
+    cats_only = count_successful_operations(Map.get(initial_correct_track, :incorrect), [:sum, :mult, :cat], correctness_tracker)
+    count_correct(initial_correct_track)+count_correct(cats_only)
   end
 
 end
--- test/day7_test.exs
+++ test/day7_test.exs
@@ -8,29 +8,37 @@ defmodule Day7Test do
   end
 
   test "Check build options correctly generated" do
-    assert Day7.build_options(10, 19, 1) == [{10, 19, :sum, 1},{10, 19, :mult, 1}]
+    assert Day7.build_options(10, 19, 1, [:sum, :mult]) == [{10, 19, :sum, 1},{10, 19, :mult, 1}]
+    assert Day7.build_options(10, 19, 1, [:sum, :mult, :cat]) == [{10, 19, :sum, 1},{10, 19, :mult, 1}, {10, 19, :cat, 1}]
   end
 
   test "Check operation decider functions properly" do
     assert Day7.operation_decider(10, 19, :sum) == 29
     assert Day7.operation_decider(10, 19, :mult) == 190
+    assert Day7.operation_decider(10, 19, :cat) == 1019
   end
 
   test "Check operation runner comes up with correct values" do
-    assert Day7.operation_runner(190,{10,19}) == true
-    assert Day7.operation_runner(3267,{81, 40, 27}) == true
-    assert Day7.operation_runner(292, {11, 6, 16, 20}) == true
-    assert Day7.operation_runner(83, {17, 5}) == false
-    assert Day7.operation_runner(156,{15, 6}) == false
-    assert Day7.operation_runner(7290, {6, 8, 6, 15}) == false
-    assert Day7.operation_runner(161011, {16, 10, 13}) == false
-    assert Day7.operation_runner(192, {17, 8, 14}) == false
-    assert Day7.operation_runner(21037, {9, 7, 18, 13}) == false
+    assert Day7.operation_runner(190,{10,19},[:sum, :mult]) == true
+    assert Day7.operation_runner(3267,{81, 40, 27},[:sum, :mult]) == true
+    assert Day7.operation_runner(292, {11, 6, 16, 20},[:sum, :mult]) == true
+    assert Day7.operation_runner(83, {17, 5},[:sum, :mult]) == false
+    assert Day7.operation_runner(156,{15, 6},[:sum, :mult]) == false
+    assert Day7.operation_runner(7290, {6, 8, 6, 15},[:sum, :mult]) == false
+    assert Day7.operation_runner(161011, {16, 10, 13},[:sum, :mult]) == false
+    assert Day7.operation_runner(192, {17, 8, 14},[:sum, :mult]) == false
+    assert Day7.operation_runner(21037, {9, 7, 18, 13},[:sum, :mult]) == false
 
+    assert Day7.operation_runner(156,{15, 6},[:sum, :mult, :cat]) == true
+    assert Day7.operation_runner(7290, {6, 8, 6, 15},[:sum, :mult, :cat]) == true
+    assert Day7.operation_runner(161011, {16, 10, 13},[:sum, :mult, :cat]) == false
+    assert Day7.operation_runner(192, {17, 8, 14},[:sum, :mult, :cat]) == true
+    assert Day7.operation_runner(21037, {9, 7, 18, 13},[:sum, :mult, :cat]) == false
   end
 
   test "Check sample is successful" do
     assert Day7.part1("data/day7_sample.txt") ==3749
+    assert Day7.part2("data/day7_sample.txt") ==11387
   end
 
 end

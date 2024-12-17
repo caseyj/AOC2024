524edd96e9867b2c852d455e0782cd1a7136eefd
John B. Casey <caseyjohnb@gmail.com>
2024-12-17 00:22:38 -0500

    day 16 getting wrong answer on real input, sample inputs are correct

--- lib/day14.ex
+++ lib/day14.ex
@@ -71,6 +71,13 @@ defmodule Day14 do
       end)
   end
 
+  def get_all_points(str,turns, size_x, size_y) do
+    Enum.reduce(str_to_point_slopes(str), [], fn pt_slope, acc ->
+      new_spot = find_spot_after_n_turns(elem(pt_slope, 0),elem(pt_slope, 1),turns,size_x,size_y)
+      acc++[new_spot]
+    end)
+  end
+
   def count_quadrants(str,turns, size_x, size_y) do
 
     points = Enum.reduce(str_to_point_slopes(str), [], fn pt_slope, acc ->
@@ -91,9 +98,9 @@ defmodule Day14 do
 
   def variance(var_list) do
     mean = div(Enum.sum(var_list), length(var_list))
-    div(Enum.reduce(var_list, 0,fn var,acc ->
+    div(floor(Enum.reduce(var_list, 0,fn var,acc ->
       acc+:math.pow((var-mean),2)
-    end), length(var_list))
+    end)), length(var_list))
   end
 
 
@@ -103,14 +110,49 @@ defmodule Day14 do
     {x_var, y_var}
   end
 
+  def variance_minima(str,size_x, size_y) do
+    Enum.reduce(0..floor(:math.pow((size_x*size_y),2)), {{100000000,10000000}, 0,[]}, fn turns, acc ->
+
+      points = get_all_points(str,turns, size_x, size_y)
+      variances = get_variances(points)
+      IO.puts("Currently on #{turns}, with detected minima #{Utils.print_tuple(elem(acc,0))}, detected variances #{Utils.print_tuple(variances)}")
+      if elem(variances,0 )< elem(elem(acc,0),0) or elem(variances,1) < elem(elem(acc,0),1) do
+        {{elem(variances,0 ), elem(variances,1 )}, turns, points}
+      else
+        acc
+      end
+    end)
+
+  end
+
+  def get_unique_count(points) do
+    length(points) - length(Enum.uniq(points))
+
+  end
+
+  def get_points_when_unique(str,size_x, size_y) do
+    Enum.reduce_while(0..floor(:math.pow((size_x*size_y),2)), { 0,[]}, fn turns, acc ->
+
+      points = get_all_points(str,turns, size_x, size_y)
+
+      #IO.puts("Currently on #{turns}, detected no overlaps #{elem(acc, 0)}")
+      if get_unique_count(points) == 0 do
+        {:halt,{turns, elem(acc,1)++points}}
+      else
+        {:cont,acc}
+      end
+    end)
+  end
+
   def part1(filename) do
     {:ok, content} = File.read(filename)
     mult_quadrants(count_quadrants(content, 100, 101,103))
   end
 
   def part2(filename) do
-    {:ok, _} = File.read(filename)
-
+    {:ok, content} = File.read(filename)
+   #variance_minima(content, 101,103)
+   "#{elem(get_points_when_unique(content, 101,103),0)}"
   end
 
 end
---
+++ test/day16_test.exs
@@ -0,0 +1,11 @@
+defmodule Day16Test do
+
+  use ExUnit.Case
+
+  test "Check part 1" do
+    assert Day16.part1("data/day16_sample1.txt") == 7036
+    assert Day16.part1("data/day16_sample2.txt") == 11048
+    assert Day16.part1("data/day16_sample3.txt") == 21148
+  end
+
+end

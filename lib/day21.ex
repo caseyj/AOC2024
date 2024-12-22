24cf803895cca7860c5798246502523e73d31b3f
John B. Casey <caseyjohnb@gmail.com>
2024-12-22 00:24:29 -0500

    Day21 in progress, an optimal pathway for each translation is needed

--- lib/day21.ex
+++ lib/day21.ex
@@ -1,14 +1,18 @@
 defmodule Day21 do
 
-  def get_directions_for_next_spot(current_spot, next_spot, walls) do
+  def instruction_length_scoring_function(_, new_point) do
+
+  end
+
+  def get_directions_for_next_spot(current_spot, next_spot, walls, size_x,size_y) do
     elem(Utils.a_star(
       [{current_spot, :east, 0, Utils.manhattan(current_spot, next_spot), []}],
       next_spot,
       [],
       walls,
       &Utils.a_star_default_scoring_function/2,
-      4,
-      3),
+      size_x+1,
+      size_y+1),
       4)
   end
 
@@ -25,16 +29,82 @@ defmodule Day21 do
     end)++["A"]
   end
 
+  def pre_computed_map_buttons(number_map, button_labels, size_x, size_y) do
+
+      mapped_labels = for x <- button_labels, y <- button_labels, x != y, do: [x,y]
+      Enum.reduce(mapped_labels, %{}, fn [x,y], acc ->
+        Map.put(acc, "#{x},#{y}", punch_in_numbers(number_map, x, y, size_x, size_y))
+      end)
+    end
+
   @doc """
   Given a map of where all the numbers are, the current_number, and the next number, plot out the robot movements
   """
-  def punch_in_numbers(number_map, current_number, next_number) do
+  def punch_in_numbers(number_map, current_number, next_number, size_x, size_y) do
     start_coord = hd(Map.get(number_map, current_number))
     end_coord= hd(Map.get(number_map, next_number))
-    get_directions_for_next_spot(start_coord, end_coord, [{3,0}])
+    walls = Map.get(number_map, "#")
+    get_directions_for_next_spot(start_coord, end_coord, walls, size_x, size_y)
     |>history_to_instructions()
   end
 
+  def instruction_translate(pre_compute, instruction) do
+    Map.get(pre_compute, instruction, [])
+  end
 
+  def instruction_reduce(instruction_list_tuple, pre_computed_buttons) do
+    Enum.reduce(Enum.to_list(0..tuple_size(instruction_list_tuple)-1), [], fn index, acc ->
+      instruction_pair = elem(instruction_list_tuple, index)
+      instruction = elem(instruction_pair, 0)
+      if index == 0 do
+        acc++punch_in_numbers(pre_computed_buttons, "A", instruction, 3,2)
+      else
+        previous_instruction = elem(elem(instruction_list_tuple, index-1), 0)
+        acc++punch_in_numbers(pre_computed_buttons, previous_instruction, instruction, 3,2)
+      end
+
+    end)
+  end
+
+  def instructions_to_instructions(instruction_list, pre_computed_buttons, depth) do
+    instruction_list_tuple = List.to_tuple(Enum.with_index(instruction_list))
+    if depth == 1 do
+      instruction_reduce(instruction_list_tuple, pre_computed_buttons)
+    else
+      instructions_to_instructions(instruction_reduce(instruction_list_tuple, pre_computed_buttons), pre_computed_buttons, depth-1)
+    end
+
+  end
+
+  def get_full_robot_list(input_number) do
+    number_pad = Utils.generate_map_from_split_str("789\n654\n321\n#0A")
+    direction_pad = Utils.generate_map_from_split_str( "#^A\n<v>")
+    {number_pad_x, number_pad_y} = {3,4}
+
+    split = String.split(input_number, "", trim: true)
+    |> Enum.with_index() |> List.to_tuple()
+    numbers_reduced = Enum.reduce(
+      Enum.to_list(0..tuple_size(split)-1),
+      [],
+      fn index, acc ->
+        if index == 0 do
+          acc++punch_in_numbers(
+            number_pad,
+            "A",
+            elem(elem(split, index),0),
+            number_pad_x,
+            number_pad_y)
+        else
+          acc++punch_in_numbers(
+            number_pad,
+            elem(elem(split, index-1),0),
+            elem(elem(split, index),0),
+            number_pad_x,
+            number_pad_y)
+      end
+    end)
+    instruction_1 = instructions_to_instructions(numbers_reduced,direction_pad)
+    instructions_to_instructions(instruction_1,direction_pad)
+  end
 
 end
--- test/day21_test.exs
+++ test/day21_test.exs
@@ -2,17 +2,75 @@ defmodule Day21Test do
 
   use ExUnit.Case
 
-  for{start_number, end_number, expected} <- [
-    {"A","0", ["<", "A"]},
-    {"0","2", ["^", "A"]},
-    {"2","9", [">","^","^","A"]},
-    {"9","A", ["v","v","v","A"]},
+  for{start_number, end_number, size_x, size_y, number_map_input,expected} <- [
+    {"A","0", 3,4, "789\n654\n321\n#0A" , [["<", "A"]]},
+    {"0","2", 3,4, "789\n654\n321\n#0A", [["^", "A"]]},
+    {"2","9", 3,4, "789\n654\n321\n#0A", [[">","^","^","A"]]},
+    {"9","A", 3,4, "789\n654\n321\n#0A", [["v","v","v","A"]]},
+    {"A","<", 3,2, "#^A\n<v>", [["v","<","<","A"],["<", "v", "<", "A"]]},
+    {"<","A", 3,2, "#^A\n<v>", [[">",">","^","A"],[">", "^", ">", "A"]]},
+    {"A","^", 3,2, "#^A\n<v>", [["<","A"]]},
+    {"^","A", 3,2, "#^A\n<v>", [[">","A"]]},
+    {"A",">", 3,2, "#^A\n<v>", [["v","A"]]},
+    {">","A", 3,2, "#^A\n<v>", [["^","A"]]},
+    {"A","v", 3,2, "#^A\n<v>", [["<","v","A"],["v","<","A"], ]},
+    {"v","A", 3,2, "#^A\n<v>", [[">","^","A"],["^",">","A"], ]},
   ] do
     test "check punch_in_numbers #{start_number}, #{end_number}" do
-      number_map = Utils.generate_map_from_split_str("789\n654\n321\n#0A")
+      number_map = Utils.generate_map_from_split_str(unquote(number_map_input))
 
-      assert Day21.punch_in_numbers(number_map,unquote(start_number), unquote(end_number)) == unquote(expected)
+      assert Enum.member?(unquote(expected),Day21.punch_in_numbers(number_map,unquote(start_number), unquote(end_number), unquote(size_x), unquote(size_y)))
     end
   end
+
+  test "Check precomputed_map_buttons" do
+    map = Day21.pre_computed_map_buttons(Utils.generate_map_from_split_str( "#^A\n<v>"), ["A","^","v", ">", "<"], 3,2)
+    for {button, expected} <- [
+      {"A,^", [["<","A"]]},
+      {"^,A", [[">","A"]]},
+      {"A,v", [
+        ["<","v","A"],
+        ["<","v","A"]]
+      },
+      {"v,A" ,[
+        [">","^","A"],
+        ["^",">","A"]
+        ]},
+     ] do
+      assert Enum.member?(expected, Map.get(map, button))
+    end
+  end
+
+  for {instructions_list,expected} <- [
+    {["^",">"], 5},
+    {["<","A","^","A",">","^","^","A","v","v","v","A"], 28},
+    {["v", "<", "<", "A", ">", ">", "^", "A", "<", "A", ">", "A", "v", "A", "<", "^", "A", "A", ">", "A", "<", "v", "A", "A", "A", ">", "^", "A"], 1, 68},
+  ] do
+    test "Check instructions_to_instructions #{expected}" do
+      map = Utils.generate_map_from_split_str( "#^A\n<v>")
+      assert length(Day21.instructions_to_instructions(unquote(instructions_list), map)) == unquote(expected)
+    end
+  end
+
+  test "Check instructions_to_instructions instructions_to_instructions" do
+    map = Utils.generate_map_from_split_str( "#^A\n<v>")
+    assert length(Day21.instructions_to_instructions(Day21.instructions_to_instructions(["<","A","^","A",">","^","^","A","v","v","v","A"], map), map)) == 68
+  end
+
+
+  for {input_str, expected_size} <- [
+   {"029A" , 68},
+   {"980A", 60},
+   {"179A", 68},
+   {"456A", 64},
+   {"379A", 64},
+  ] do
+    test "get_full_robot_list #{input_str}" do
+      robot_list = Day21.get_full_robot_list(unquote(input_str))
+      assert length(robot_list) == unquote(expected_size)
+    end
+  end
+
+
 
 end
